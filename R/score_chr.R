c97680cf55ba8fbdc311769435c1bc056ac2f41b
Jose M Muino <jose.muino@wur.nl>
2010-02-12 13:42:01 +0000

    ppois modified
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CSAR@44682 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/score_chr.R
+++ R/score_chr.R
@@ -8,7 +8,10 @@ sa.nnucs<-sum(sa)
 con.nnucs<-sum(con)
 ##Apply quantile normalization ???
 la=mean(con[con>0]);la<-max(1,la,backg)####Minimum coverage
-if(test=="Poisson"){for (i in 1:length(sa)){la2<-max(con[i]*sa.nnucs/con.nnucs,la);con[i]<- -(ppois(sa[i],la2,lower.tail=FALSE,log.p=TRUE))}}
+if(test=="Poisson"){
+con<-con*sa.nnucs/con.nnucs
+con[con<la]<-la
+con<- -(ppois(sa,con,lower.tail=FALSE,log.p=TRUE))}
 if(test=="Ratio"){con[con<la]<-la;con<-sa/con}
 return(con)
 }
