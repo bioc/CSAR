a40dc6613796db1397764c97f65d6c1c31c8a9a9
Jose M Muino <jose.muino@wur.nl>
2010-02-12 17:35:16 +0000

    distance2Genes improved
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CSAR@44688 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/distance2Genes.R
+++ R/distance2Genes.R
@@ -2,30 +2,30 @@
 function(win,gff,t= -log(0.05),d1=-3000,d2=1000){
 win<-win[win$score>t,]
 ## I can not know the final length of res, it will depends about how close the peaks and genes are.
-res<-c()
+peakName<-c();p1<-c();p2<-c();score<-c();gene<-c();le<-c();
 
 for (chr in unique(as.character(win$chr))){
 tempwin<-win[as.character(win$chr)==chr,]
 tempgff<-gff[as.character(gff$V1)==chr,]
-
-
+tempgff$V9<-as.character(tempgff$V9)
 for (i in 1:length(tempwin$posPeak)){
-
-name<-paste(chr,tempwin$posPeak[i],sep="_")
-tempPos1<-tempwin$posPeak[i]-tempgff$V4
-tempPos2<-tempwin$posPeak[i]-tempgff$V5
-temp<-tempPos1
-tempPos1[tempgff$V7=="-"]<-tempPos2[tempgff$V7=="-"]* -1
-tempPos2[tempgff$V7=="-"]<-temp[tempgff$V7=="-"]* -1
-rm(temp)
+pos=tempwin$posPeak[i]
+name<-paste(chr,pos,sep="_")
+tempPos1<-pos-tempgff$V4
+tempPos2<-pos-tempgff$V5
+whichPos<-(tempgff$V7=="-")
+temp<-tempPos1[whichPos]
+tempPos1[whichPos]<-tempPos2[whichPos]* -1
+tempPos2[whichPos]<-temp*-1
 whichPos=(tempPos1>=d1 & tempPos2<=d2)
-if(is.element(TRUE,whichPos)){
-t<-data.frame(peakName=name,p1=tempPos1[whichPos],p2=tempPos2[whichPos],score=tempwin$score[i], gene=as.character(tempgff$V9[whichPos]),le=abs((tempgff$V4-tempgff$V5)[whichPos]))
-res<- rbind(res,t)
+temp<-tempgff[whichPos,]
+peakName<-c(peakName,rep(name,length(temp$V9)))
+p1<-c(p1,tempPos1[whichPos])
+p2<-c(p2,tempPos2[whichPos])
+score<-c(score,rep(tempwin$score[i],length(temp$V9)))
+gene<-c(gene,temp$V9)
+le<-c(le,abs(temp$V4-temp$V5))
 }
 }
-
-}
-
-return(res)
+return(data.frame(peakName,p1,p2,score,gene,le))
 }
--- R/genesWithPeaks.R
+++ R/genesWithPeaks.R
@@ -1,17 +1,19 @@
 `genesWithPeaks` <-
 function(distances){
-gene<-unique(as.character(distances$gene))
-max=integer(length(gene));u3000=max;u2000=max;u1000=max;d0=max;d1000=max;
-for (i in 1:length(gene)){
-temp<-distances[distances$gene==gene[i],]
-max[i]=max(temp$score[temp$p2< 1000 & temp$p1>= -3000])
-u3000[i]=max(c(0,temp$score[temp$p1< -2000 & temp$p1>= -3000]))
-u2000[i]=max(c(0,temp$score[temp$p1< -1000 & temp$p1>= -2000]))
-u1000[i]=max(c(0,temp$score[temp$p1< -0 & temp$p1>= -1000]))
-d0[i]=max(c(0,temp$score[temp$p1>=0 & temp$p2<=0]))
-d1000[i]=max(c(0,temp$score[temp$p2>0 & temp$p2<1000]))
-}
-fin=data.frame(name=unique(as.character(distances$gene)),max3kb1kb=max,u3000,u2000,u1000,d0,d1000)
+indices <- split(seq_len(nrow(distances)), distances$gene)
+distances$score[distances$p1<3000 & distances$p2>=1000]<-0
+max3kb1kb=as.numeric(lapply(indices,function(x){max(c(0,distances$score[x]))}))
+temp<-distances$score;temp[ distances$p1>= -2000]<-0
+u3000=as.numeric(lapply(indices,function(x){max(c(0,temp[x]))}))
+temp<-distances$score;temp[distances$p1< -2000 | distances$p1>= -1000]<-0
+u2000=as.numeric(lapply(indices,function(x){max(c(0,temp[x]))}))
+temp<-distances$score;temp[distances$p1< -1000 | distances$p1>= 0]<-0
+u1000=as.numeric(lapply(indices,function(x){max(c(0,temp[x]))}))
+temp<-distances$score;temp[distances$p1< 0 | distances$p2>= 0]<-0
+d0=as.numeric(lapply(indices,function(x){max(c(0,temp[x]))}))
+temp<-distances$score;temp[distances$p2< 0 ]<-0
+d1000=as.numeric(lapply(indices,function(x){max(c(0,temp[x]))}))
+fin=data.frame(name=as.character(labels(indices)),max3kb1kb,u3000,u2000,u1000,d0,d1000)
 return(fin)
 }
 
