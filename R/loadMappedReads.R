7d586ef106a1ceb54257eeb6e626bb59a1b84700
Jose M Muino <jose.muino@wur.nl>
2010-04-20 08:31:01 +0000

    LoadMappedRead faster
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CSAR@46249 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/loadMappedReads.R
+++ R/loadMappedReads.R
@@ -1,19 +1,19 @@
+
 `loadMappedReads` <-
 function(file,format="SOAP",header=FALSE){
-
+skip<-0;if(header){skip<-1}
+minimum<-c("Nhits","lengthRead","strand","chr","pos")
 ###Different formats: SOAP, user defined...
 if(length(format)==1){
-if(format=="SOAP"){
-output<-read.table(file=file,comment.char = "",colClasses = "character",fill=TRUE,header=header,flush=TRUE)
-output<-data.frame(Nhits=as.integer(output$V4),lengthRead=as.integer(output$V6),strand=output$V7,chr=output$V8,pos=as.integer(output$V9))
-}
-}
-else{
-output<-read.table(file=file,comment.char = "",col.names=format,colClasses = rep("character",length(format)),fill=FALSE,header=header,flush=TRUE)
+if(format=="SOAP"){format<-c("name", "sequence","error","Nhits","other","lengthRead","strand","chr","pos")}}
+if(length(format[is.element(format,minimum)])<length(minimum)){stop(paste("Format should contain at least the column names:",minimum))}
+what<-vector("list",length(format))
+what[is.element(format,minimum)]<-"character"
+output<-scan(file=file,what=what,flush=TRUE,comment.char="",skip=skip)
+names(output)<-format
 output<-data.frame(Nhits=as.integer(output$Nhits),lengthRead=as.integer(output$lengthRead),strand=output$strand,chr=output$chr,pos=as.integer(output$pos))
-}
-message(paste(length(output$pos),"reads loaded from file:",file))
 gc(verbose=FALSE)
 return(output)
 }
+
 
